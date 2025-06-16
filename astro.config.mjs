c332b91efc31eb74d482c16cf43bc828ffaccd13
Amol Khatri <amolkhatri@gmail.com>
2025-06-16 16:36:12 +0530

    added output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amolkhatri.github.io',
   base: '/astro-notion-blog-starter',
+  output: 'static',
   integrations: [mdx(), sitemap()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
