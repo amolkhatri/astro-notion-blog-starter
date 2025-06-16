fa5e495a66bb8a87c48cf327182d5316873c8848
Amol Khatri <amolkhatri@gmail.com>
2025-06-16 16:15:27 +0530

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-notion-blog-starter.github.io',
-	base: '/demo',
-	
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-notion-blog-starter.github.io',
+  base: '/demo',
+  integrations: [mdx(), sitemap()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
