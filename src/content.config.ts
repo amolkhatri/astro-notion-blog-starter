ba2385e68216cf31129228c657763f6bc917c49f
Amol Khatri <amolkhatri@gmail.com>
2025-09-15 17:11:13 +0530

    Added logging for Notion API key and database ID in content configuration file.

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,12 @@ import { loadEnv } from 'vite';
 
 const env = Object.assign({}, loadEnv(import.meta.env.MODE, process.cwd(), ''), process.env);
 
+let notionApiKey = import.meta.env.NOTION_API_KEY ?? '';
+let notionDatabaseId = import.meta.env.NOTION_DATABASE_ID ?? '';
+
+console.log(" static notionApiKey", notionApiKey);
+console.log(" static notionDatabaseId", notionDatabaseId);
+
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
